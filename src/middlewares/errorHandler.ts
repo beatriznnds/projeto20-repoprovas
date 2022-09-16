ec314531c39d20a7dcbb1e4e73c5ad629958fb7a
beatriz.nnds <beatriz.nnds@gmail.com>
2022-09-16 13:27:05 -0300

    fix: error handler

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,25 +1,23 @@
 import { Request, Response, NextFunction } from "express";
 
-interface error {
-  type: "Not Found" | "Conflict" | "Unauthorized" | "ValidationError";
-  message: string;
-}
-
 export default function errorHandler(
-  error: error,
+  error: any,
   req: Request,
   res: Response,
   next: NextFunction
 ) {
-  const errorTypes = {
-    "Not Found": 404,
-    Conflict: 409,
-    Unauthorized: 401,
-    ValidationError: 422,
-  };
-
-  return res.status(errorTypes[error.type]).send({ message: error.message });
-
+  if (error.type === "Not Found") {
+    return res.status(404).send({ message: error.message });
+  }
+  if (error.type === "Conflict") {
+    return res.status(409).send({ message: error.message });
+  }
+  if (error.type === "Unauthorized") {
+    return res.status(401).send({ message: error.message });
+  }
+  if (error.type === "ValidationError") {
+    return res.status(422).send({ message: error.message });
+  }
   console.log(error);
   res.sendStatus(500);
 }
